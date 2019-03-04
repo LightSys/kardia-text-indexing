c10ad994c44db9b802dc2ced157b21cd7388b9e6
Walter <wcuddington@cedarville.edu>
2019-03-04 14:49:10 -0700

    Added the 'step 1' as specified in this file

---
+++ tokenizer.py
@@ -0,0 +1,13 @@
+# File description:
+#
+# 1.  Get long string from importer
+# 2.  Split long string into multiple strings based on
+#     the newline character
+# 3. Tokenize the words from the array of strings
+
+import nltk
+
+def tokenizer(imported_string):
+    string_array = imported_string.split("\n")
+    
+    return words;
