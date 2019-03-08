1482257b876e80a783dbc364a7f9f1b58d5b0c4d
walter <wcuddington@cedarville.edu>
2019-03-07 20:46:31 -0700

    added temp file functionality for the img importer and the img-based pdf importer

--- importers/img_pdf_importer.py
+++ importers/img_pdf_importer.py
@@ -2,29 +2,32 @@ from __future__ import print_function
 from wand.image import Image
 import img_importer as scan
 import subprocess
+import tempfile
 
 def importer(filename):
     contents = ''
-    page_num = 0
-    with Image(filename=filename) as img:
-        page_num = len(img.sequence)
-        with img.convert('png') as converted:
-            converted.save(filename='out/out.png')
+    with tempfile.TemporaryDirectory() as tmpdir:
+        page_num = 0
+        with Image(filename=filename) as img:
+            page_num = len(img.sequence)
+            with img.convert('png') as converted:
+                converted.save(filename=tmpdir+'/out.png')
 
-    if page_num == 1:
-        subprocess.call(['convert', 'out/out.png', '-filter', 'point', '-resize', '380%', 'out/out.png'])
-    else:
-        for x in range(page_num):
-            input = 'out/out-' + str(x) + '.png'
-            subprocess.call(['convert', input, '-filter', 'point', '-resize', '380%', input])
+        if page_num == 1:
+            subprocess.call(['convert', tmpdir+'/out.png', '-filter', 'point', '-resize', '380%', tmpdir+'/out.png'])
+        else:
+            for x in range(page_num):
+                input = tmpdir+'/out-' + str(x) + '.png'
+                subprocess.call(['convert', input, '-filter', 'point', '-resize', '380%', input])
 
-    if page_num == 1:
-        contents = scan.importer('out/out.png', 'png')
-    else:
-        for x in range(page_num):
-            input = 'out/out-' + str(x) + '.png'
-            contents = contents + scan.importer(input, 'png')
+        if page_num == 1:
+            contents = scan.importer(tmpdir+'/out.png', 'png')
+        else:
+            for x in range(page_num):
+                input = tmpdir+'/out-' + str(x) + '.png'
+                contents = contents + scan.importer(input, 'png')
     return contents;
 
-UwU = importer('../test_files/Professional_Test_PDF.pdf')
-print(UwU)
+# test run:
+# UwU = importer('../test_files/test.pdf')
+# print(UwU)
