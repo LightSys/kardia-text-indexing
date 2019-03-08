3d1c1917ba3855091f04eb3dd84593e58b17b7aa
walter <wcuddington@cedarville.edu>
2019-03-07 20:10:28 -0700

    add temp file functionality for doc and text pdf importers

--- importers/pdf_importer.py
+++ importers/pdf_importer.py
@@ -6,11 +6,9 @@ def importer(filename):
     with tempfile.NamedTemporaryFile() as temp:
         subprocess.call(['pdftotext', filename, temp.name])
         temp.seek(0)
-        # file = open(, 'r')
         contents = temp.read().decode("utf-8")
-        # file.close()
     return contents;
 
 # test run:
-UwU = importer('../test_files/test.pdf')
-print(UwU)
+# UwU = importer('../test_files/test.pdf')
+# print(UwU)
