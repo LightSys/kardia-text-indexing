8a99b68d9260f37bd6e07e6b9f087f40606c280c
walter <wcuddington@cedarville.edu>
2019-03-07 17:33:43 -0700

    added directory watcher

---
+++ watcher.py
@@ -0,0 +1,22 @@
+import time
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+class MyHandler(FileSystemEventHandler):
+    def __init__(self, f):
+        self.callback = f
+    def on_any_event(self, event):
+        self.callback()
+
+def watch(f, path)
+    event_handler = MyHandler(f)
+    observer = Observer()
+    observer.schedule(event_handler, path=path, recursive=True)
+    observer.start()
+
+    try:
+        while True:
+            time.sleep(10))
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
