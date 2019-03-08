d547f4666835bdfbdf063c62ed64609ee9d009bb
walter <wcuddington@cedarville.edu>
2019-03-08 09:57:07 -0700

    Made code more readable

--- importers/doc_importer.py
+++ importers/doc_importer.py
@@ -5,14 +5,16 @@ import os
 def importer(filename):
     contents = ''
     with tempfile.TemporaryDirectory() as tmpdir:
-        f = open(tmpdir+'/file.txt', "w")
-        subprocess.call(['soffice', '--cat', filename], stdout=f)
-        f.close()
-        f = open(tmpdir+'/file.txt', "r")
-        contents = f.read()
-        f.close()
+        # make output file in temp directory to write to
+        output_file = open(tmpdir+'/file.txt', "w")
+        subprocess.call(['soffice', '--cat', filename], stdout=output_file)
+        # close file and reopen it in read mode
+        output_file.close()
+        output_file = open(tmpdir+'/file.txt', "r")
+        contents = output_file.read()
+        output_file.close()
     return contents;
 
 # test run:
-# UwU = importer('../test_files/test.odt')
+# UwU = importer('../test_files/Professional_Test_Doc.docx')
 # print(UwU)
