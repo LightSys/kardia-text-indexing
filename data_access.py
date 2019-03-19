ff8e674666a7e25c963726c541d34171280ca126
cmd16 <catherine.dejager10@gmail.com>
2019-03-19 16:56:38 -0600

    Add spaces to sql syntax

--- data_access.py
+++ data_access.py
@@ -187,12 +187,12 @@ class MySQLDataAccessor:
         self.pending_occurrences = []
 
         self.database.cursor().executemany(
-            "with w as (select e_word_id from e_text_search_word where e_word = %s),"
-                    "tw as (select e_word_id from e_text_search_word where e_word = %s)"
-               "insert into e_text_search_rel"
-                   "(e_word_id, e_target_word_id, e_rel_relevance,"
-                    "s_date_created, s_created_by, s_date_modified, s_modified_by)"
-               "select w.e_word_id, tw.e_word_id, %s, now(), %s, now(), %s"
+            "with w as (select e_word_id from e_text_search_word where e_word = %s), "
+                    "tw as (select e_word_id from e_text_search_word where e_word = %s) "
+               "insert into e_text_search_rel "
+                   "(e_word_id, e_target_word_id, e_rel_relevance, "
+                    "s_date_created, s_created_by, s_date_modified, s_modified_by) "
+               "select w.e_word_id, tw.e_word_id, %s, now(), %s, now(), %s "
                "from w, tw",
             map(lambda t: (t[0], t[1], t[2], username, username), self.pending_relationships))
         self.database.commit()
