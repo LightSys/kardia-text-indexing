914d65b4f8c2cc242a8880dbe6780f2440b5d9b8
andrewthomas <andrewdt97@gmail.com>
2019-03-19 16:02:31 -0600

    Importing: Refactor document imports

--- importers/doc_importer.py
+++ importers/doc_importer.py
@@ -1,20 +1,20 @@
-import subprocess
+'''
+author: Andrew Thomas
+Code-a-thon 2019
+'''
+import textract
 import tempfile
-import os
+from bs4 import UnicodeDammit
 
-def importer(filename):
-    contents = ''
-    with tempfile.TemporaryDirectory() as tmpdir:
-        # make output file in temp directory to write to
-        output_file = open(tmpdir+'/file.txt', "w")
-        subprocess.call(['soffice', '--cat', filename], stdout=output_file)
-        # close file and reopen it in read mode
-        output_file.close()
-        output_file = open(tmpdir+'/file.txt', "r")
-        contents = output_file.read()
-        output_file.close()
+def importer(file_path):
+    text = UnicodeDammit(textract.process(file_path)).unicode_markup
+    lines = text.split('\n')
+    contents = []
+    for line in lines:
+        if line != '':
+            contents.append(line.split())
     return contents;
 
-# test run:
-# UwU = importer('../test_files/Professional_Test_Doc.docx')
-# print(UwU)
+if __name__ == '__main__':
+    UwU = importer('test_files/Professional_Test_Doc.odt')
+    print(UwU[0])
