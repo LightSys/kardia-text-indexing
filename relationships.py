0a237c8255324aff0bccb691cab011f34afc308a
cmd16 <catherine.dejager10@gmail.com>
2019-03-21 16:23:56 -0600

    Add break statements for synsets below threshold, more debugging statements

--- relationships.py
+++ relationships.py
@@ -17,7 +17,7 @@ def add_relationships_synset(word, synset, relevance, data_accessor):
         print("synset %s lemma %s relevance %f" % (synset.name(), lemma.name(), relevance))
         data_accessor.add_relationship(word, lemma.name(), relevance)
         for form in lemma.derivationally_related_forms():
-            print("sysnset %s lemma %s related form %s" % (synset.name(), lemma.name(), form.name()))
+            print("synset %s lemma %s related form %s" % (synset.name(), lemma.name(), form.name()))
             data_accessor.add_relationship(word, form.name(), relevance - 0.01)
 
 def add_relationships(word, data_accessor, threshold = 0.5):
@@ -28,23 +28,34 @@ def add_relationships(word, data_accessor, threshold = 0.5):
         add_relationships_synset(word, syn, 0.99, data_accessor)
         for hypernym in syn.closure(lambda s: s.hypernyms()):
             relevance = hypernym.path_similarity(syn)
+            if relevance < threshold:
+                break
+            print("adding hypernym", hypernym)
             add_relationships_synset(word, hypernym, relevance, data_accessor)
         for hyponym in syn.closure(lambda s: s.hyponyms()):
             relevance = hyponym.path_similarity(syn)
+            if relevance < threshold:
+                break
+            print("adding hyponym", hyponym)
             add_relationships_synset(word, hyponym, relevance, data_accessor)
         for part_meronym in syn.part_meronyms():
             relevance = part_meronym.path_similarity(syn)
+            print("adding part meronym", part_meronym)
             add_relationships_synset(word, part_meronym, relevance, data_accessor)
         for substance_meronym in syn.substance_meronyms():
             relevance = substance_meronym.path_similarity(syn)
+            print("adding substance meronym", substance_meronym)
             add_relationships_synset(word, substance_meronym, relevance, data_accessor)
         for part_holonym in syn.part_holonyms():
             relevance = part_holonym.path_similarity(syn)
+            print("adding part holonym", part_holonym)
             add_relationships_synset(word, part_holonym, relevance, data_accessor)
         for substance_holonym in syn.substance_holonyms():
             relevance = substance_holonym.path_similarity(syn)
+            print("adding substance holonym", substance_holonym)
             add_relationships_synset(word, substance_holonym, relevance, data_accessor)
         for entailment in syn.entailments():
             relevance = entailment.path_similarity(syn)
+            print("adding entailment", entailment)
             add_relationships_synset(word, entailment, relevance, data_accessor)
 
