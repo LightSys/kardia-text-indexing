db10856f580d6720f5955b486bb5889fb1398aaf
James <jamespeapen@gmail.com>
2019-03-21 18:31:35 -0400

    Added documentation explaining the print statement

--- show_words_in_document.py
+++ show_words_in_document.py
@@ -2,5 +2,5 @@ import data_access
 
 data_accessor = data_access.MySQLDataAccessor()
 for word in data_accessor.get_all_words():
-    print(word.text, word.relevance)
+    print(word.text, word.relevance) #prints the indexed words and their assigned relevances
 
