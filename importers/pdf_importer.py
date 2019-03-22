ebce7b27f934403cee9d308281032bb050377ad9
andrewthomas <andrewdt97@gmail.com>
2019-03-22 13:31:17 -0600

    Importers: Combine pdf import to one method

--- importers/pdf_importer.py
+++ importers/pdf_importer.py
@@ -1,14 +1,40 @@
 import subprocess
 import tempfile
+import textract
+from bs4 import UnicodeDammit
+import sys
+from wand.image import Image
+import img_importer
+import os
 
-def importer(filename):
+def textBasedImport(file_path):
+    text = UnicodeDammit(textract.process(file_path)).unicode_markup
+    return text
+
+def imageBasedImport(file_path):
     contents = ''
-    with tempfile.NamedTemporaryFile() as temp:
-        subprocess.call(['pdftotext', filename, temp.name])
-        temp.seek(0)
-        contents = temp.read().decode("utf-8")
-    return contents;
+    with tempfile.TemporaryDirectory() as tmpdir:
+        page_num = 0
+        with Image(filename=file_path, resolution=400) as img:
+            page_num = len(img.sequence)
+            with img.convert('png') as converted:
+                converted.save(filename=tmpdir+'/out.png')
 
-# test run:
-# UwU = importer('../test_files/test.pdf')
-# print(UwU)
+        if page_num == 1:
+            contents = img_importer.importer(tmpdir+'/out.png', 'png')
+        else:
+            for x in range(page_num):
+                input = tmpdir+'/out-' + str(x) + '.png'
+                contents = contents + img_importer.importer(input, 'png')
+
+    return contents
+
+def importer(file_path):
+    text = textBasedImport(file_path)
+    if not text.strip():
+        text = imageBasedImport(file_path)
+    return text
+
+if __name__ == '__main__':
+    UwU = importer(sys.argv[1])
+    print(UwU)
