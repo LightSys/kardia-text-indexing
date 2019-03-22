52371d738118d3f81188912acc828b7c6ab081ac
James <jamespeapen@gmail.com>
2019-03-22 13:49:11 -0400

    Updated documentation

--- show_words_in_document.py
+++ show_words_in_document.py
@@ -1,6 +1,9 @@
 import data_access
 
+"""
+prints the indexed words in the database and their assigned relevances
+"""
 data_accessor = data_access.MySQLDataAccessor()
 for word in data_accessor.get_all_words():
-    print(word.text, word.relevance) #prints the indexed words and their assigned relevances
+    print(word.text, word.relevance) 
 
